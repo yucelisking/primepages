16e5b94a95db45b22215dc4a9bb3a621f9ca6f41
Yücel <yucelisbrn@gmail.com>
2024-04-15 13:25:51 +0200

    create python pages

---
+++ makeprimepages2.py
@@ -0,0 +1,48 @@
+def is_prime(num):
+  """
+  Check if the given number is prime.
+  """
+  if num <= 1:
+    return False
+  elif num <= 3:
+    return True
+  elif num % 2 == 0 or num % 3 == 0:
+    return False
+  i = 5
+  while i * i <= num:
+    if num % i == 0 or num % (i + 2) == 0:
+      return False
+    i += 6
+  return True
+
+def prime_numbers(start, count):
+  """
+  Generate prime numbers within the given range (inclusive start, exclusive end).
+  """
+  num = start
+  prime_list = []
+  while len(prime_list) < count:
+    if is_prime(num):
+      prime_list.append(num)
+    num += 1
+  return prime_list
+
+def format_list(item_list):
+  """
+  Format the given list as a Markdown bullet list.
+  """
+  return "\n".join([f"* {item}" for item in item_list])
+
+def create_page(start, count, filename):
+  """
+  Generate and save a Markdown page listing prime numbers within the given range.
+  """
+  prime_list = format_list(prime_numbers(start, count))
+  with open(filename, "w") as file:
+    file.write(prime_list)
+
+if __name__ == "__main__":
+  create_page(1, 100, "primes1.md")
+  create_page(101, 100, "primes101.md")
+  create_page(201, 100, "primes201.md")
+  # ... copy and modify the code to create more pages as needed.
